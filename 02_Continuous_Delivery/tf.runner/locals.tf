57a29b680339d81423396019ca1320c04b18bafa
siarheisaroka <admin@techplace.one>
2023-05-09 18:57:45 +0300

    task2 clean up

--- 02_Continuous_Delivery/tf.runner/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  name    = var.studentname
-  surname = var.studentsurname
-  regions = var.regions
-  sa      = data.google_service_account.def-sa.email
-  # vpc     = data.google_compute_network.tf_vpc.name
-  project = data.google_project.current.id
-  # subnets = [data.google_compute_subnetwork.us-central1.id, data.google_compute_subnetwork.us-east1.id]
-}
--- unreg_runner.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo gitlab-runner unregister --all-runners
-sudo -u gitlab-runner gitlab-runner unregister --all-runners
